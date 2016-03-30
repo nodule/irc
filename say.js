39512e4781d3ffcdec2554fdc81334ba9c12d61f
Rob Halff <rob.halff@gmail.com>
2016-03-30 12:31:49 +0200

    update

--- say.js
+++ say.js
@@ -17,7 +17,7 @@ module.exports = {
         title: "Message",
         type: "string",
         async: true,
-        fn: function __IN__(data, x, source, state, input, output) {
+        fn: function __IN__(data, source, state, input, $, output) {
           var r = function() {
             $.bot.say($.target, $.in);
           }.call(this);
